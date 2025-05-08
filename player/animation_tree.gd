60b5261eacefe981a493de75827590cf8978791d
saltchicken <john.eicher89@gmail.com>
2025-05-08 15:46:30 -0700

    Add set_speed_scale

--- player/animation_tree.gd
+++ player/animation_tree.gd
@@ -5,3 +5,6 @@ func play(anim_name):
 	
 func set_direction(anim_name, direction):
 	set("parameters/" + anim_name + "/BlendSpace2D/blend_position", direction)
+
+func set_speed_scale(anim_name, speed_scale):
+	set("parameters/" + anim_name + "/TimeScale/scale", speed_scale)
