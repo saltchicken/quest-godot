db73b638521a7b269c92ecc986554f1096b5bd01
saltchicken <john.eicher89@gmail.com>
2025-05-10 12:09:33 -0700

    Fix offset from character for fireball

--- player/states/cast.gd
+++ player/states/cast.gd
@@ -13,7 +13,7 @@ func enter_server(_state_packet):
 	casting = true
 	var direction = Direction.look_direction_to_world_direction(%InputComponent.look_direction, owner)
 
-	GameManager.cast_projectile(owner.global_position + Vector3.UP * 0.5 + direction / 4, direction, owner.PROJECTILE_SPEED, owner)
+	GameManager.cast_projectile(owner.global_position + Vector3.UP * 0.5 + direction / 2, direction, owner.PROJECTILE_SPEED, owner)
 
 func update_server(delta:float):
 	input_dir = %InputComponent.input_direction
