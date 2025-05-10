1cbd8c839f097c7b1394e35bfe16346a6cec5c96
saltchicken <john.eicher89@gmail.com>
2025-05-10 11:40:06 -0700

    Fireball ready changed to run on server only

--- fireball.gd
+++ fireball.gd
@@ -4,6 +4,7 @@ var direction = Vector3.FORWARD
 var speed = 10.0
 
 func _ready() -> void:
-	apply_impulse(direction * speed)
-	var timer = get_tree().create_timer(5.0)
-	timer.timeout.connect(queue_free)
+	if multiplayer.is_server():
+		apply_impulse(direction * speed)
+		var timer = get_tree().create_timer(5.0)
+		timer.timeout.connect(queue_free)
